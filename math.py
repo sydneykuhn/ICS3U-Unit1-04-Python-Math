0c0ee4464b6e1de3bf683e7367b3b2a8e9a3e926
Ubuntu <ubuntu@ip-172-31-90-143.ec2.internal>
2021-09-11 16:41:00 +0000

    added area and primeter of a 5x3 rectangle.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Sept 2020
+# This program calculates the area and perimeter of a rectangle 
+#    with dimentions of 5cm and 3cm 
+
+
+def main():
+# this function calculates the area and perimeter
+
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm2.".format(5 * 3))
+    print("Perimeter is {}cm.".format(2 * (5 + 3)))
+
+
+
+if __name__ == "__main__":
+    main()
